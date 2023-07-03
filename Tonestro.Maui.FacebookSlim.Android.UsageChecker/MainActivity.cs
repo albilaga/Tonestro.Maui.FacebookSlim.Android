fface9f7f53170f655635f66542404f06d90bbf6
albilaga <albilaga@pm.me>
2023-07-03 21:47:00 +0700

    add java

---
+++ Tonestro.Maui.FacebookSlim.Android.UsageChecker/MainActivity.cs
@@ -0,0 +1,17 @@
+using Com.Tonestro.Facebookslim;
+
+namespace Tonestro.Maui.FacebookSlim.Android.UsageChecker;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+        
+        var   _tracker = AppEventsLoggerSlim.NewLogger(ApplicationContext);
+    }
+}
